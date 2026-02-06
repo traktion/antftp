cb90739172018d28b0eec7295ed181ef0ccca910
Paul Green <traktion0@gmail.com>
2026-02-06 08:46:22 +0000

    feat: replace skeleton unftp-sbe-anttp code with AntTP gRPC integration

--- crates/unftp-sbe-anttp/src/ext.rs
+++ crates/unftp-sbe-anttp/src/ext.rs
@@ -1,11 +1,10 @@
-use crate::Filesystem;
+use crate::Anttp;
 use libunftp::auth::DefaultUser;
 use libunftp::{Server, ServerBuilder};
-use std::path::PathBuf;
 
 /// Extension trait purely for construction convenience.
 pub trait ServerExt {
-    /// Create a new `Server` with the given Autonomi archive root pointer address.
+    /// Create a new `Server` with the given AntTP address.
     ///
     /// # Example
     ///
@@ -13,18 +12,15 @@ pub trait ServerExt {
     /// use libunftp::Server;
     /// use unftp_sbe_anttp::ServerExt;
     ///
-    /// let server = Server::with_anttp("<address>");
+    /// let server = Server::with_anttp();
     /// ```
-    fn with_anttp<P: Into<PathBuf> + Send + 'static>(path: P) -> ServerBuilder<Filesystem, DefaultUser> {
-        let p = path.into();
+    fn with_anttp() -> ServerBuilder<Anttp, DefaultUser> {
         libunftp::ServerBuilder::new(Box::new(move || {
-            let p = &p.clone();
-            match Filesystem::new(p) {
-                Ok(fs) => fs,
-                Err(e) => panic!("Cannot open file system root {}: {}", p.display(), e),
-            }
+            tokio::runtime::Handle::current().block_on(async {
+                Anttp::new().await.expect("Cannot connect to AntTP")
+            })
         }))
     }
 }
 
-impl ServerExt for Server<Filesystem, DefaultUser> {}
+impl ServerExt for Server<Anttp, DefaultUser> {}
