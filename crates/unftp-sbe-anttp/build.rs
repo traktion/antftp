e5d9a925e52c4b2f6bc36b08cd7df4764361f10d
Paul Green <traktion0@gmail.com>
2026-02-12 19:46:31 +0000

    Update build.rs to use protos within crate.

--- crates/unftp-sbe-anttp/build.rs
+++ crates/unftp-sbe-anttp/build.rs
@@ -3,8 +3,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let protoc_exists = Command::new("protoc").arg("--version").output().is_ok();
 
     if protoc_exists {
-        tonic_build::compile_protos("../../proto/public_archive.proto")?;
-        tonic_build::compile_protos("../../proto/pointer.proto")?;
+        tonic_build::compile_protos("proto/public_archive.proto")?;
+        tonic_build::compile_protos("proto/pointer.proto")?;
     } else {
         println!("cargo:warning=protoc not found, disabling gRPC support");
         println!("cargo:rustc-cfg=grpc_disabled");
